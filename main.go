d3529bc8580495e1578bb115092b096a9a1d2e39
stylo <pathanfaraaz@gmail.com>
2020-12-02 13:33:33 -0800

    Cleaned up code. Added more comments

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ var client, err = mongo.Connect(context.TODO(), clientOptions)
 var collection = client.Database("test").Collection("trainers")
 
 
-// Get all trainers
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| Get all trainers
 func getTrainers(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   findOptions := options.Find()
@@ -61,7 +61,7 @@ func getTrainers(w http.ResponseWriter, r *http.Request) {
   json.NewEncoder(w).Encode(results)
 }
 
-// Get specific trainers
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| Get single trainers
 func getTrainer(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   params := mux.Vars(r) //Get the params
@@ -78,7 +78,7 @@ func getTrainer(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// Create trainer
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| Create trainer
 func createTrainer(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
 
@@ -105,7 +105,7 @@ func createTrainer(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//Delete trainer
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| Delete trainer
 func deleteTrainer(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   params := mux.Vars(r) //Get the params
@@ -121,11 +121,11 @@ func deleteTrainer(w http.ResponseWriter, r *http.Request) {
     return
   }
 
-  log.Printf("Deleted document with name: %v from the trainers collection.\n", params["name"])
+  log.Printf("Deleted user: %v from the trainers collection.\n", params["name"])
 
 }
 
-//Update trainer
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| Update trainer
 func updateTrainer(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json")
   params := mux.Vars(r) //Get the params
@@ -151,12 +151,10 @@ func updateTrainer(w http.ResponseWriter, r *http.Request) {
               updateResult.MatchedCount,
               updateResult.ModifiedCount)
 
-
 }
 
 func main() {
   //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| CONNECTION TO MONGODB
-
     //checking the connection
   err = client.Ping(context.TODO(), nil)
   if err != nil {
@@ -182,116 +180,6 @@ func main() {
   //Starting the server
   log.Fatal(http.ListenAndServe(":3000", r))
 
-
-/* ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| BEGIN MONGODB WORK
-
-// Set client options
-  clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-  // Connect to MongoDB
-  client, err := mongo.Connect(context.TODO(), clientOptions)
-
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  //checking the connection
-  err = client.Ping(context.TODO(), nil)
-
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Println("Connected to MongoDB! owo")
-
-  //can now get a handle for the trainers collection
-  // will later use this handle to query the collection
-  collection := client.Database("test").Collection("trainers")
-
-  //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| CREATE
-  //create structs to insert into the DB
-  //NAME(string), AGE(int), CITY(string)
-  jeff := Trainer{"Jeff Winger", 40, "Greendale"}
-  abed := Trainer{"Abed Nadir", 21, "Greendale"}
-  troy := Trainer{"Troy Barnes", 21, "Greendale"}
-
-  //inserting a single document
-  insertResult, err := collection.InsertOne(context.TODO(), jeff)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Println("Inserted a single document: ", insertResult.InsertedID)
-
-  //inserting multiple, InsertMany() will take a slice of objects
-  trainers := []interface{}{abed, troy}
-
-  insertManyResult, err := collection.InsertMany(context.TODO(), trainers)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
-
-  //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| UPDATE
-
-  // updating a single document. Filter document used to match doc,
-  // and update doc describes the update operation. Here, using bson.D types
-
-  jeffFilter := bson.D{{"name", "Jeff Winger"}}
-
-  update := bson.D{
-    {"$inc", bson.D{
-      {"age", 1},
-      }},
-  }
-
-  updateResult, err := collection.UpdateOne(context.TODO(), jeffFilter, update)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Printf("Matched %v documents and updated %v documents.\n",
-              updateResult.MatchedCount,
-              updateResult.ModifiedCount)
-
-
-  //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| READ
-  // to find one doc, need filter doc
-  // and pointer to value in which the result can be decoded.
-
-  // value into which result will be decoded
-  var result Trainer
-
-  err = collection.FindOne(context.TODO(), jeffFilter).Decode(&result)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Printf("Found a single document %+v\n", result)
-
-  //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| DELETE
-
-  //collection.Drop() to delete an entire collection
-
-  //DeleteMany or DeleteOne
-  deleteResult, err := collection.DeleteMany(context.TODO(), jeffFilter)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  fmt.Printf("Deleted %v documents in the trainers collection.\n", deleteResult)
-
-  //closing the connection
-  err = client.Disconnect(context.TODO())
-
-  if err != nil {
-      log.Fatal(err)
-  }
-
-  collection.Drop(context.TODO())
-  fmt.Println("Connection to MongoDB closed. uwu")
-  |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| */
 } // MAIN
 
 
